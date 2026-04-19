fa14c4c3a77f783ca41ba4ac2012091255ebf9ac
Kairat Kubatov <kairat.kubatov_2028@ucentralasia.org>
2026-04-19 10:35:45 +0600

    feat: add place data files for Sulaiman-Too and Manas Ordo

---
+++ src/data/places/index.js
@@ -0,0 +1,9 @@
+import sulaimanToo from "./sulaiman-too";
+import manasOrdo from "./manas-ordo";
+
+const PLACES = {
+  "sulaiman-too": sulaimanToo,
+  "manas-ordo": manasOrdo,
+};
+
+export default PLACES;
